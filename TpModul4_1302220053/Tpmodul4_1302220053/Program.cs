7562e2cf228b2a1b7990200d434806c8a9fdd3e2
AdsGab <gabrielleadsf@gmail.com>
2024-03-13 00:00:39 +0700

    menambahkan implementasi dengan state-based

--- TpModul4_1302220053/Tpmodul4_1302220053/Program.cs
+++ TpModul4_1302220053/Tpmodul4_1302220053/Program.cs
@@ -16,6 +16,14 @@ public class Program
             int postalCode = getKodePoes(county);
             Console.WriteLine(county +": " + postalCode);
         }
+        Pintu objPintu = new Pintu();
+        Console.WriteLine(objPintu.currentState);
+        objPintu.ActivateTrigger(Trigger.KunciPintu);
+        Console.WriteLine(objPintu.currentState);
+        objPintu.ActivateTrigger(Trigger.bukaPintu);
+        Console.WriteLine(objPintu.currentState);
+        
+
     }
 }
 
@@ -29,3 +37,55 @@ public class kodePos
     }
 }
  
+public  enum pintuState { TERKUNCI, TERBUKA};
+public enum Trigger {bukaPintu, KunciPintu};
+public class Pintu
+{
+    public class DoorMachine
+    {
+        public pintuState stateAwal;
+        public pintuState stateAkhir;
+        public Trigger trigger;
+
+        public DoorMachine(pintuState stateAwal, pintuState stateAkhir, Trigger trigger)
+        {
+            this.stateAwal = stateAwal;
+            this.stateAkhir = stateAkhir;
+            this.trigger = trigger;
+        }
+
+    }
+        DoorMachine[] transisi =
+        {
+        new DoorMachine(pintuState.TERBUKA, pintuState.TERKUNCI, Trigger.KunciPintu),
+        new DoorMachine(pintuState.TERKUNCI, pintuState.TERBUKA, Trigger.bukaPintu),
+         };
+
+        public pintuState currentState = pintuState.TERKUNCI;
+
+        public pintuState GetNextState(pintuState stateAwal, Trigger trigger)
+        {
+            pintuState stateAkhir = stateAwal;
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                DoorMachine perubahan = transisi[i];
+                if (stateAwal == perubahan.stateAwal && trigger == perubahan.trigger)
+                {
+                    stateAkhir = perubahan.stateAkhir;
+                }
+            }
+            return stateAkhir;
+        }
+
+        public void ActivateTrigger(Trigger trigger)
+        {
+            currentState = GetNextState(currentState, trigger);
+            Console.WriteLine("State anda Sekarang adalah : " + currentState);
+            if (currentState == pintuState.TERBUKA)
+            {
+                Console.WriteLine("Pintu tidak Terkunci");
+            } else if(currentState == pintuState.TERKUNCI) {
+            Console.WriteLine("Pintu Terkkunci");
+        }
+        }
+    }
